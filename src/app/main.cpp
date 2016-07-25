cc5968e40e75983462e5f28ab68054292e77e838
Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
2016-07-25 20:52:52 +0300

    catch exceptions in main in order to properly destruct all resources

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,15 +1,29 @@
 /*
- * Copyright 2015 Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
+ * Copyright 2015, 2016 Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
  */
 
 #include "App.hpp"
 #include "Log.hpp"
+#include <glcxx/src/gl.hpp>
 
 /// @brief entry point
 int main()
 {
-   CApp app;
-   Log::msg("main loop start");
-   app.run();
-   Log::msg("main loop end");
+   glcxx::set_glerror_cb([](const std::stringstream& func, const char* scope, GLenum err)
+                         {
+                            Log::err("gl error code ", err, " in ", func.rdbuf(), " at ", scope);
+                         });
+   // catch and rethrow to properly free all gl resources even in case of
+   // exception
+   try
+   {
+      CApp app;
+      Log::msg("main loop start");
+      app.run();
+      Log::msg("main loop end");
+   }
+   catch(std::exception& e)
+   {
+      Log::err(e.what());
+   }
 }
