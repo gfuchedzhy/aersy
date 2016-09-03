4f9df623988b9dd82d150b418ff0c8b1fa39a950
Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
2016-09-04 01:24:40 +0300

    replace sfml-graphics with imagemagick++

--- src/engine/Texture.cpp
+++ src/engine/Texture.cpp
@@ -3,19 +3,34 @@
  */
 
 #include "Texture.hpp"
-#include <SFML/Graphics/Image.hpp>
 #include "Log.hpp"
+
+#define MAGICKCORE_QUANTUM_DEPTH 8
+#define MAGICKCORE_HDRI_ENABLE 0
+#include <Magick++.h>
+
+namespace
+{
+   static struct CInitImageMagick
+   {
+         CInitImageMagick()
+         {
+            Magick::InitializeMagick(nullptr);
+         }
+   } initMagick;
+}
 
 CTexture::CTexture(const std::string& filename)
    : texture(GL_TEXTURE_2D)
 {
    Log::msg("loading texture from ", filename);
-   sf::Image img;
-   if (!img.loadFromFile(std::string("aersy-res/") + filename))
-      throw std::runtime_error{"failed to load texture"};
-   auto size = img.getSize();
+   Magick::Image img;
+   img.read(std::string("aersy-res/") + filename);
+   auto size = img.size();
+   Magick::Blob blob;
+   img.write(&blob, "RGBA");
    bind();
-   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, (void*)img.getPixelsPtr());
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.width(), size.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, blob.data());
    glGenerateMipmap(_target);
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
