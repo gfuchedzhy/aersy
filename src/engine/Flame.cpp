34d56422d88489b66ccee691c058832436f1e3be
Grygorii Fuchedzhy <gfuchedzhy@gmail.com>
2016-11-19 22:14:13 +0200

    fix draw_arrays signature

--- src/engine/Flame.cpp
+++ src/engine/Flame.cpp
@@ -32,6 +32,6 @@ void CJetFlame::draw(const CContext& context) const
       p.set<cts("uTexture")>(mTexture);
       glcxx::disable_depth_mask_guard dtLock;
       glcxx::enable_blending_guard bLock(GL_SRC_ALPHA, GL_ONE);
-      p.draw_arrays(mVAO, aliveParticleNum(), GL_POINTS);
+      p.draw_arrays(mVAO, GL_POINTS, 0, aliveParticleNum());
    }
 }
